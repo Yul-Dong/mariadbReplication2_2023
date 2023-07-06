094e5f765059e6f00b64621887309f01861d912c
Donghyun <dev.dhson@gmail.com>
2023-07-06 09:48:40 +0900

    프로젝트 생성

---
+++ src/main/java/com/ll/mariadbreplication/MariadbReplicationApplication.java
@@ -0,0 +1,13 @@
+package com.ll.mariadbreplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariadbReplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MariadbReplicationApplication.class, args);
+    }
+
+}
