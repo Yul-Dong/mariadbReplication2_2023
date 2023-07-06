66616e7cf50e1d2ff4e26c9d1e1c2134e9c4207d
Donghyun <dev.dhson@gmail.com>
2023-07-06 09:59:08 +0900

    post 패키지

---
+++ src/main/java/com/ll/mariadbreplication/domain/post/PostRepository.java
@@ -0,0 +1,6 @@
+package com.ll.mariadbreplication.domain.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
